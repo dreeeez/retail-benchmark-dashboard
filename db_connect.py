59b730803b28371fdc6e1e8847b36dcebf6d0dc1
Marco Schneider <marco.schn@outlook.com>
2025-11-20 15:53:38 +0100

    Setting up Database connection

---
+++ db_connect.py
@@ -0,0 +1,17 @@
+import json
+import pyodbc
+
+def get_connection():
+    with open("config.json") as f:
+        cfg = json.load(f)
+
+    conn_str = (
+        "DRIVER={ODBC Driver 18 for SQL Server};"
+        f"SERVER={cfg['server']};"
+        f"DATABASE={cfg['database']};"
+        f"UID={cfg['user']};"
+        f"PWD={cfg['password']};"
+        "Encrypt=no;"
+    )
+
+    return pyodbc.connect(conn_str)
